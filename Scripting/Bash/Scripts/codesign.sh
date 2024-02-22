09eef82f233d2b138fa8b791d6f4ff969cb82413
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-02-22 08:14:20 +0500

    Update codesign.sh

--- Scripting/Bash/Scripts/codesign.sh
+++ Scripting/Bash/Scripts/codesign.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+# Sign code or binaries with GPG for integrity verification
+
+echo "Enter the path to the file to sign:"
+read file_to_sign
+
+echo "Enter the GPG key ID for code signing:"
+read signing_key
+
+gpg --detach-sign --local-user $signing_key $file_to_sign
