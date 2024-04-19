1ae1df3a4822125d879a946ee0d451322e991d39
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-19 21:27:08 +0500

    Create Basics.sh

---
+++ Linux/Commands/Basics.sh
@@ -0,0 +1,23 @@
+# BASIC COMMANDS
+
+# Displays the current time and date
+date
+# Displays a calendar of the current month.
+cal
+# Display the current amount of free space on our disk drives
+df
+# Display the amount of free memory
+free
+# Ending a terminal session
+exit
+
+# BASIC NAVIGATION
+
+# Display current working directory
+pwd
+# List the contents of a directory
+ls
+# Change directory
+cd
+
+
