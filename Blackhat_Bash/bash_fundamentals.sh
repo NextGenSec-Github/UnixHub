f3e2dc50686fa14f1dc17b8ed56013544b87495c
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-08-25 22:09:33 +0500

    Update and rename fundamentals.sh to bash_fundamentals.sh

--- Blackhat_Bash/bash_fundamentals.sh
+++ Blackhat_Bash/bash_fundamentals.sh
@@ -28,6 +28,7 @@ echo "${root_directory}"
 
 unset myvar # Unassign variable
 
+# ------------------------------
 # Control Operators
 & # Sends a command to the background.
 && # Used as a logical AND. The second command in the expression will be evaluated only if the first command evaluates to true.
@@ -43,10 +44,29 @@ touch test && touch test123 # The && operator allows us to perform an AND operat
 (ls; ps) # The () operator allows us to group commands so they act a single unit when we need to redirect them together
 ls; ps; whoami # The ; operator allows us to run multiple commands regardless of their exit status
 lzl || echo "the lzl command failed" # The || operator allows us to chain commands together using an OR operation
+# -------------------------------
 
+# -------------------------------
+# Redirection Operators
+> # Redirects stdout to a file
+>> # Redirects stdout to a file by appending it to the existing content
+&> or >& # Redirects stdout and stderr to a file
+&>> # Redirects stdout and stderr to a file by appending them to the existing content
+< # Redirects input to a commands
+| # Redirects output of a command as input to another command
+<<EOF
+This is a heredoc example.
+It can span multiple lines.
+EOF
 
-
-
+# Redirection Operator Examples
+echo "Hello World!" > output.txt # We redirect the standard output stream to a file named output.txt
+echo "Goodbye :)" >> output.txt # Appending standard output stream to file named output.txt
+ls -l / &> stderr_and_stdout.txt # Redirect both the standard output stream and the standard error stream to a file. Nothing will output to the terminal
+ls -l / 1> stdout.txt 2> stderr.txt # Sending stdout and stderr to two different files
+cat < output.txt  # Supply the contents of output.txt as input to the cat command.
+ls -l / | grep "bin" # The pipe operator (|) redirects the output of one command and uses it as the input of another
+# -------------------------------
 
 
 
