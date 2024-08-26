d40ef198681994b9e5d2c8fcd8256f0d95e7aa90
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-08-26 19:35:22 +0500

    Create int_comparison.sh

---
+++ Blackhat_Bash/scripts/int_comparison.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+INT1="10"
+INT2="20"
+
+if [[ "${INT1}" -gt "${INT2}" ]]; then
+  echo "${INT1} is greater than ${INT2}"
+else
+  echo "${INT1} is less than or equal to ${INT2}"
+fi
+
+
+
